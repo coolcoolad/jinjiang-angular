f337b070f0cb6a642b41a98473d559cfb858039b
matthew <matthewgan@126.com>
2018-11-12 12:46:43 +0800

    update docker but not working

--- bund18/src/environments/environment.prod.ts
+++ bund18/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  recordUrl: 'http://localhost:8000/api/bund18/record/',
+  statUrl: 'http://localhost:8000/api/bund18/stat/',
+  remoteUrl: 'http://tunnel40011.remotuino.fullrank.cc:81/arduino/digital/',
 };
