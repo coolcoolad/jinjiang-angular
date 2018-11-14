53c0d73b161dc9e1a2ff474edd22c436a41e5819
matthew <matthewgan@126.com>
2018-11-14 23:10:02 +0800

    fix prod env bug

--- bund18/src/environments/environment.prod.ts
+++ bund18/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  recordUrl: 'http://47.101.159.35/:8000/api/bund18/record/',
-  statUrl: 'http://47.101.159.35/:8000/api/bund18/stat/',
+  recordUrl: 'http://47.101.159.35:8000/api/bund18/record/',
+  statUrl: 'http://47.101.159.35:8000/api/bund18/stat/',
   remoteUrl: 'http://tunnel40011.remotuino.fullrank.cc:81/arduino/digital/',
 };
