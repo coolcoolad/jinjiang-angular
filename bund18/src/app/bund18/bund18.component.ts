6f87781552e90bd6d6c84564da0faaff259ca5c7
matthew <matthewgan@126.com>
2018-11-15 19:01:44 +0800

    update images and test shake

--- bund18/src/app/bund18/bund18.component.ts
+++ bund18/src/app/bund18/bund18.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, OnDestroy } from '@angular/core';
 import { first } from 'rxjs/operators';
 import { RecordService } from '../_services/record.service';
 import { RemoteControlService } from '../_services/remote-control.service';
 import { Record, RecordPost } from '../_models';
 import { Router } from '@angular/router';
+import * as shake from 'shake.js';
+import { ValueTransformer } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-bund18',
@@ -11,19 +13,36 @@ import { Router } from '@angular/router';
   styleUrls: ['./bund18.component.css']
 })
 
-export class Bund18Component implements OnInit {
+export class Bund18Component implements OnInit, OnDestroy {
 
   constructor(
     private recordService: RecordService,
     private remoteControlService: RemoteControlService,
     private router: Router,
-  ) { }
+    private zone: NgZone
+  ) { }  
 
-
+  public CalledFromOutside(newValue:string){
+    this.zone.run(()=>{
+      console.log(newValue);
+      alert("works");
+    });
+  }
+  
   ngOnInit() {
+    console.log("test", shake);     
+    window['angularComponentRef'] = {
+      zone: this.zone,
+      componentFn: (value)=>this.CalledFromOutside(value),
+      component: this,
+    };
   }
 
-  onClickShake() {
+  ngOnDestroy() {
+
+  }
+
+  public onClickShake() {
     console.log("on click shake button");
     var new_record: RecordPost = {
       device: 'bund18',
